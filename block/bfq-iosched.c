194c5b9f52c2b51f6085c050c979956a23562071
Piotr Gorski <lucjan.lucjanov@gmail.com>
2022-08-18 15:42:01 +0200

    bfq-6.1: set CachyOS branding
    Signed-off-by: Piotr Gorski <lucjan.lucjanov@gmail.com>

--- block/bfq-iosched.c
+++ block/bfq-iosched.c
@@ -7578,6 +7578,7 @@ MODULE_ALIAS("bfq-iosched");
 static int __init bfq_init(void)
 {
 	int ret;
+	char msg[60] = "BFQ I/O-scheduler: BFQ-CachyOS v6.1";
 
 #ifdef CONFIG_BFQ_GROUP_IOSCHED
 	ret = blkcg_policy_register(&blkcg_policy_bfq);
@@ -7608,6 +7609,11 @@ static int __init bfq_init(void)
 	ret = elv_register(&iosched_bfq_mq);
 	if (ret)
 		goto slab_kill;
+
+#ifdef CONFIG_BFQ_GROUP_IOSCHED
+	strcat(msg, " (with cgroups support)");
+#endif
+	pr_info("%s", msg);
 
 	return 0;
 
